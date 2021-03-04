4a20fc5272f5e822517582b3e5d7c953b94066a9
Marco Todaro <marcoposta59@gmail.com>
2021-03-04 11:00:59 +0100

    ad changes

--- components/AdBanner.js
+++ components/AdBanner.js
@@ -9,14 +9,13 @@ const AdBanner = () => {
   const productionID = "ca-app-pub-5618837515872371/9432038202";
   // Is a real device and running in production.
   const adUnitID = Constants.isDevice && !__DEV__ ? productionId : testID;
-  /*
+
   useEffect(() => {
     const setDev = async () => {
       await setTestDeviceIDAsync("EMULATOR");
     };
     setDev();
   }, []);
-*/
   const bannerError = (e) => {
     console.log(e);
   };
